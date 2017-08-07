fee39eff39a3ffe41c55a29d99e9309e7ac5e49b
Thomas van Dam <t.vandam@wearetriple.com>
2017-08-07 12:10:23 +0200

    Add CI scripts and tokens

---
+++ tests/NativeHls.test.js
@@ -0,0 +1,24 @@
+import '@meisterplayer/meister-mock';
+import NativeHls from '../src/js/NativeHls';
+
+const PLUGIN_NAME = 'NativeHls';
+const SUPPORTED_TYPES = ['m3u8', 'm3u'];
+
+describe('Hls class', () => {
+    test(`pluginName should be ${PLUGIN_NAME}`, () => {
+        expect(NativeHls.pluginName).toBe(PLUGIN_NAME);
+    });
+
+    test('pluginVersion should return a version string', () => {
+        // Version should match the SemVer pattern (e.g. 2.11.9)
+        expect(NativeHls.pluginVersion).toMatch(/\d+\.\d+\.\d+/);
+    });
+});
+
+describe('The rest of the test suite', () => {
+    test('It should be written', () => {
+        const test = { testsWritten: false };
+
+        expect(test).toEqual({ testsWritten: true });
+    });
+});
